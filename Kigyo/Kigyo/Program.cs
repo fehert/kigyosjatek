30330270c0f3beb6926684829ad27f6643acdcc4
Fehér Tamás <93767193+fehert@users.noreply.github.com>
2022-09-07 17:08:08 +0200

    Add files via upload

---
+++ Kigyo/Kigyo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kigyo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hány almát evett meg a kígyó?");
+            int alma = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hány körtét evett meg a kígyó?");
+            int korte = Convert.ToInt32(Console.ReadLine());
+
+            int hossz = alma * 10 + korte * 20;
+
+            Console.WriteLine("A kígyó hossza: {0}",hossz);
+        }
+    }
+}
